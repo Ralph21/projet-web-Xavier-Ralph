dda0b8d8c635896394fba70e69cb659dd290ee58
xababa_mac <xababa_mac@10.117.151.122>
2016-01-25 16:39:45 +0100

    - change into index.html

--- Dev Web Project/src/main/java/com/miage/webController.java
+++ Dev Web Project/src/main/java/com/miage/webController.java
@@ -1,17 +1,15 @@
 package com.miage;
 
-import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 public class webController {
 
-		@RequestMapping(value = "/index", method = RequestMethod.GET)
-		public String AccessIndex(Model model,RedirectAttributes redirectAttributes,HttpSession session) 
+		@RequestMapping(value = "/index")
+		public String AccessIndex() 
 		{				
 			return "index";
 		}
