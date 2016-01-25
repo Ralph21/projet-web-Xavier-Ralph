942b53de70ba48ec79ac84beb50ec7dc1ecc684e
Ralph <ralph.gaume@gmail.com>
2016-01-25 21:44:13 +0100

    désactivation de spring security (retrait du POM) et correction de index.html

--- Dev Web Project/src/main/java/com/miage/webController.java
+++ Dev Web Project/src/main/java/com/miage/webController.java
@@ -3,15 +3,29 @@ package com.miage;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
 public class webController {
 
+	
+		/**
+		 * Lors de l'appel du chemin /index, lance index.html
+		 */
 		@RequestMapping(value = "/index")
 		public String AccessIndex() 
 		{				
 			return "index";
 		}
+		
+		/**
+		 * par default, lance la page index.html
+		 */
+	    @RequestMapping("/")
+	    public String retourIndex(RedirectAttributes redirectAttributes)
+	    {
+	    	return "index";
+	    }
 	
 }
