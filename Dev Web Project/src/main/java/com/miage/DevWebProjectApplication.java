b1deb0e09a49d092a0c498eb2fd75563fac290df
Ralph <ralph.gaume@gmail.com>
2016-01-25 15:56:42 +0100

    création du projet

---
+++ Dev Web Project/src/main/java/com/miage/DevWebProjectApplication.java
@@ -0,0 +1,12 @@
+package com.miage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevWebProjectApplication.class, args);
+	}
+}
---
+++ Dev Web Project/src/test/java/com/miage/DevWebProjectApplicationTests.java
@@ -0,0 +1,18 @@
+package com.miage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DevWebProjectApplication.class)
+@WebAppConfiguration
+public class DevWebProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
