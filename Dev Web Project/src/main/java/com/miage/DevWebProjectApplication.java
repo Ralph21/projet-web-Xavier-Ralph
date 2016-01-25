3e76832b2b6f9456a174de60d635d1d6590412b2
xababa_mac <xababa_mac@10.117.151.122>
2016-01-25 16:29:06 +0100

    - add index.html - add line into devwebprojectapplication.java

--- Dev Web Project/src/main/java/com/miage/DevWebProjectApplication.java
+++ Dev Web Project/src/main/java/com/miage/DevWebProjectApplication.java
@@ -2,9 +2,16 @@ package com.miage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class DevWebProjectApplication {
+public class DevWebProjectApplication  extends WebMvcConfigurerAdapter {
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/index").setViewName("index");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevWebProjectApplication.class, args);
