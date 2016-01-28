7fb9683cde703c48d04302fa000e820e5d90a8a0
Ralph21 <ralph.gaume@gmail.com>
2016-01-29 00:50:43 +0100

    Inscription : Création d'un formulaire d'inscription avec un modèle utilisateur et une verification, pour le moment ne fais que vérifier si les champs son bien remplis. Accessible depuis Espace Client --> Connexion/Inscription.
    Modification d'un sous-menu de la barre supérieure dans index.html
    (ajout de commantaires explicatifs)

---
+++ Dev Web Project/src/main/java/com/miage/Utilisateur.java
@@ -0,0 +1,64 @@
+package com.miage;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Utilisateur {
+
+	//TODO : ajouter id ?
+	
+	// Le Prénom doit être relativement long 
+ 	//    mais pas trop court non plus.
+    @Size(min=2, max=30)
+    private String firstName;
+	
+    // Idem pour le Nom.
+    @Size(min=2, max=30)
+    private String lastName;
+    
+
+    // L'utilisateur doit spécifier son âge
+    @NotNull
+    @Min(18)
+    private Integer age;
+    
+    // TODO: add validation annotation
+    private String sexe;
+
+    
+
+    public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getSexe() {
+		return sexe;
+	}
+
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+
+	public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+}
