c0a5154e4a667cda22a87d2f39eb74531230d187
Ralph21 <ralph.gaume@gmail.com>
2016-02-08 23:37:15 +0100

    Refactoring && netoyage projet
    Préparation d'une base plus propre du projet pour le nouveau système mis
    en place dans des POC.

--- Dev Web Project/src/main/java/com/miage/webController.java
+++ Dev Web Project/src/main/java/com/miage/webController.java
@@ -1,8 +1,5 @@
 package com.miage;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,37 +9,18 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class webController {
 
-	@Autowired
-	carRepository car_bdd;
-
-	/**
-	 * Lors de l'appel du chemin /index, lance index.html
-	 */
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String AccessIndex(Model model,RedirectAttributes redirectAttributes) {
-		
-		//avant d'afficher la page index on récupère tous les car de la BDD
-		List<car> car = (List<car>) car_bdd.findAll();
-		model.addAttribute("car_list", car);
-		
+	public String AccessIndex(Model model,RedirectAttributes redirectAttributes) {		
 		return "index";
 	}
 
-	/**
-	 * par default, lance la page index.html
-	 */
+
 	@RequestMapping("/")
 	public String retourIndex(Model model,RedirectAttributes redirectAttributes) {
-		
-		//avant d'afficher la page index on récupère tous les car de la BDD
-		List<car> car = (List<car>) car_bdd.findAll();
-		model.addAttribute("car_list", car);
 		return "index";
 	}
 
-	/**
-	 * Lors de l'appel du chemin /index, lance index.html
-	 */
+
 	@RequestMapping(value = "/contact")
 	public String AccessContact() {
 		return "contact";
