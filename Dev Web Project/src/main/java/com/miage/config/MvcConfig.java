3537d0e4ea2585f1e3e3bc25d98ca5d76b27ffd4
Ralph21 <ralph.gaume@gmail.com>
2016-02-12 00:40:10 +0100

    Commit spring security [SOUCIS D'ACCES AUX RESSOURCES]
    Si tu te poses la question Xavier de pourquoi le style est hs c'est à
    cause de spring security.

---
+++ Dev Web Project/src/main/java/com/miage/config/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.miage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/contact").setViewName("contact");
+        registry.addViewController("/informations").setViewName("informations");
+    }
+
+}
---
+++ Dev Web Project/src/main/java/com/miage/config/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.miage.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	 @Override
+     public void configure(WebSecurity web) throws Exception {
+       web
+         .ignoring()
+            .antMatchers("/resources/**"); // #3
+     }
+	
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            	.antMatchers("/", "/index").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/index")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+}
