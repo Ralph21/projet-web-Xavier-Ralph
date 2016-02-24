4d09bca61d9e8b7df3e672b72fa0695a9f79dd38
xababa_mac <xababa_mac@pc92.home>
2016-02-24 02:38:02 +0100

    - gestionInfo updated - glyph sur le bouton supprimer (page gestionUsers) - glyph sur le bouton déconnexion - bouton suivant/annuler sur la page voiture.html - page suivante summary.html (ajout du controller ds controllerCar)

--- Dev Web Project/src/main/java/com/miage/controllers/CarController.java
+++ Dev Web Project/src/main/java/com/miage/controllers/CarController.java
@@ -3,6 +3,7 @@ package com.miage.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.miage.repositories.CarRepository;
 import com.miage.services.CarService;
@@ -19,5 +20,11 @@ public class CarController {
 	@Autowired
 	public void setCarService(CarService carService){
 		this.carService = carService;
+	}
+	
+	
+	@RequestMapping(value = "/summary")
+	public String AccessSummary() {
+		return "summary";
 	}
 }
